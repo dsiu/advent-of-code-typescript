01eff1188c4698c5d6f040747d6aab03fc2c75c4
Danny Siu <danny.siu@gmail.com>
2025-05-05 16:27:08 +0800

    2024 Day 3 DONE - added error handling

--- src/2024/Day03/AOC2024_Day03.js
+++ src/2024/Day03/AOC2024_Day03.js
@@ -1,19 +1,17 @@
+import { Option } from 'effect';
 import * as _ from 'lodash-es';
 const calMultiplication = (str) => {
     const mulPattern = /mul\(\d+,\d+\)/g;
     const argsPattern = /(\d+),(\d+)/;
-    const muls = str.match(mulPattern);
-    const result = muls?.map(args => {
-        const r = argsPattern.exec(args);
-        return [Number(r && r[1]), Number(r && r[2])];
-    });
-    return result && result.map(args => {
-        return args.reduce(_.multiply, 1);
-    });
+    return Option.fromNullable(str.match(mulPattern)).pipe(Option.map(muls => muls
+        .map(args => Option.fromNullable(argsPattern.exec(args)).pipe(Option.filter(r => r[1] != null && r[2] != null), Option.map(r => [Number(r[1]), Number(r[2])])))
+        .filter(Option.isSome)
+        .map(opt => opt.value)
+        .map(args => args.reduce(_.multiply, 1))));
 };
-const part1 = (memory) => _.sum(calMultiplication(memory));
+const part1 = (memory) => _.sum(Option.getOrThrowWith(calMultiplication(memory), () => new Error('No multiplication found')));
 const part2 = (memory) => _.sum(memory.split('do()')
-    .map(x => calMultiplication(x.split('don\'t()')[0]))
+    .map(x => Option.getOrThrowWith(calMultiplication(x.split('don\'t()')[0]), () => new Error('No multiplication found')))
     .map(_.sum));
 const parse = (data) => data.trim();
 const solvePart1 = (data) => {
